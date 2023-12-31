b0345c5e20c8d1e1b20c1120ed0864506f805a76
per.nielsen1@outlook.com <per.nielsen1@outlook.com>
2023-12-31 16:27:47 +0100

    pandas merge, select and groupby examples

--- python/hello.py
+++ python/hello.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import shutil
 import datetime
+import ebcdic
 excel_dir='/mnt/c/users/perni/OneDrive/Documents/PythonTest'
 config_dir='/home/perni/ExcelCrypto/python/config'
 out_dir='/home/perni/ExcelCrypto/python/output'
@@ -42,20 +43,23 @@ def build_big(numc1, numc2):
     c1_df.to_csv(big_dir + '/' + 'C1.csv', sep=';', encoding='utf-8', index=False)
     c2_df.to_csv(big_dir + '/' + 'C2.csv', sep=';', encoding='utf-8', index=False)
     c3_df.to_csv(big_dir + '/' + 'C3.csv', sep=';', encoding='utf-8', index=False)
-
     print("Done")
+
 def log(msg):
     now = datetime.datetime.now()
     print(msg, now.strftime("%Y-%m-%d, %H:%M:%S"))
 
+
 def write_row(out_file, rec_type, id, name, addr1): 
     record = (
+            
                   rec_type +  
                   str(id).zfill(10) +  
                   "{:<40}".format(name)[0:40] + 
                   "{:<40}".format(addr1)[0:40] 
             )
     encoded = bytearray(record.encode('utf-8'))
+#   encoded = bytearray(record.encode('cp1143'))
     out_file.write(encoded)
         
 
@@ -69,8 +73,7 @@ def load_big():
 
     c3_df = pd.read_csv(big_dir + '/' + 'C3.csv', sep= ';')
     log("after c3:")
-    
-    
+
     c1_c3_df = pd.merge(c1_df, c3_df, on='ID', how='left', indicator = 'merge_result')
     log("after join c1 C3")
 
@@ -99,16 +102,45 @@ def load_big():
 def load_config():
     config_df = pd.read_csv('python/config/my_config.csv', sep = ';')
     return dict(config_df.values)
-   
+
+def pandas_test():
+    acc =  {'acc': ['A1', 'A2', 'A3'],
+            'pt': ['P10', 'P20', 'P30']}
+    trn =  {'entity':[101, 101, 101], 'acc': ['A1', 'A1', 'A2'], 
+          'amount_gc': [100, 200, 300], 'amount_org':[200, 400, 600], 
+          'dummy_number':[1, 2, 3]}
+    df_trn = pd.DataFrame(data=trn)
+    df_acc = pd.DataFrame(data=acc)
+    print(df_trn)
+    print(df_acc)
+    # example of left join i.e. merge in pandas speak
+    df_acc_trn =pd.merge(df_acc, df_trn, on='acc', how='left', indicator = 'merge_result')
+    print(df_acc_trn)
+    # how to select rows
+    # https://stackoverflow.com/questions/17071871/how-do-i-select-rows-from-a-dataframe-based-on-column-values
+    # select those with value > 100 
+    df_acc_trn_gt100 = df_acc_trn.loc[df_acc_trn['amount_gc'] > 100 ]
+    # select those with value > 100 and account = A1
+    print(df_acc_trn_gt100)
+
+    df_acc_trn_gt100_and_a1 = df_acc_trn.loc[(df_acc_trn['amount_gc'] > 100) & 
+                                             (df_acc_trn['acc'] == 'A1') ]
+
+    print(df_acc_trn_gt100_and_a1)
+    # example of summarising (group by)
+    df_trn_sum = df_trn.groupby(['entity', 'acc']).agg({'amount_gc':'sum','amount_org':'sum'})
+    print(df_trn_sum)
+
+
 print('current directory:' + os.getcwd())
 file_name = 'cust.xlsx'
 clear_dir(out_dir)
 config = load_config()
 big_dir=config["input_dir"]
 
-build_big(int(config["numc1"]), int(config["numc2"]))
-load_big()
- 
+# build_big(int(config["numc1"]), int(config["numc2"]))
+# load_big()
+pandas_test()   
 exit(0)
  
 # importing the module
