3ac4f61de36a0338e6b89b803d5ba15a1420cb3c
per.nielsen1@outlook.com <per.nielsen1@outlook.com>
2024-12-21 17:15:39 +0100

    excel example with formulas and ranges

--- python/test_xlsxwriter.py
+++ python/test_xlsxwriter.py
@@ -2,6 +2,8 @@
 # https://dev.mysql.com/doc/connector-python/en/connector-python-example-cursor-select.html
 #---------------------------------------------------------------------------------------------
 import xlsxwriter
+from xlsxwriter.utility import xl_rowcol_to_cell
+
 import pandas as pd
 excel_dir='/mnt/c/users/perni/OneDrive/Documents/PythonTest'
 
@@ -30,9 +32,68 @@ def readExcelAndCreateExcel():
     # all done
     workbook.close()
 
+#----------------------------------------------------------
+# apply formula for a number of rows
+#--------------------------------------------------------
+def apply_formula(ws, formula, column, start_row, num_rows):
+    for row_no in range(num_rows):
+       new_formula = formula.replace("#",str(start_row + row_no + 1))
+       ws.write_formula(start_row + row_no, column, "=" + new_formula)
+       ws.write(start_row + row_no, column + 1, new_formula)
+
+#----------------------------------------------------------
+# dataframe_to_sheet 
+#--------------------------------------------------------
+def dataframe_to_sheet(df, wb, sheet_name):
+  ws = wb.add_worksheet(sheet_name)
+
+  # write column headings in first rowdata number 0
+  cur_col = 0 
+  column_names = df.columns
+  for col in column_names:
+      ws.write(0, cur_col, col)
+      cur_col = cur_col + 1 
+  # loop through dataframe row by row and column by column write to cell in Excel
+  cur_row = 1  # start after the header row     
+  for index, row in df.iterrows():
+      cur_col = 0 
+      for key, value in row.items():
+        ws.write(cur_row, cur_col, value)
+        cur_col  = cur_col + 1
+      cur_row = cur_row + 1  
+  # create range
+  last_cell = xl_rowcol_to_cell(cur_row - 1, cur_col-1, True, True)
+  wb.define_name("Range_" + sheet_name, "=" + sheet_name + "!" + "$A$2:"  + last_cell)
+
+#----------------------------------------------------------------
+# pandas to Excel
+#----------------------------------------------------------------
+def pandas_to_excel():
+  t1 = pd.DataFrame(
+    {
+      "key": ["k1", "k2", "k3"],
+	    "desc": ["Keyval 1 from t1", "Keyval 2 from t1", "keyval 3 from t1"], 
+	    "num": [ 1.0, 2.1, 3]
+    }
+  )
+  t2 = pd.DataFrame(
+    {
+      "key": ["k1", "k2", "k3"],
+	    "desc": ["Keyval 1 from t2", "Keyval 2 from t2", "keyval 3 from t2"], 
+	    "key_to_t1": [ "k1", "k3", "K2"]
+    }
+  )
+  out_Excel = excel_dir + "/pandas_to_excel.xlsx"
+  wb = xlsxwriter.Workbook(out_Excel)
+  dataframe_to_sheet(t1, wb, "t1")     
+  dataframe_to_sheet(t2, wb, "t2")     
+  ws_t2 = wb.get_worksheet_by_name("t2")
+  formula="VLOOKUP(C#,Range_t1,2,FALSE)"
+  apply_formula(ws_t2, formula,3,1,len(t2))
+  wb.close()
 
 #--------------------------
 # here we go
 #--------------------------
-
-readExcelAndCreateExcel()
+pandas_to_excel()
+# readExcelAndCreateExcel()
