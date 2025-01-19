c792b46b0be2f53b5de0398a203ac5ae28ad725e
per.nielsen1@outlook.com <per.nielsen1@outlook.com>
2025-01-19 17:18:25 +0100

    gui examples

--- python/test_gui/fromwin.sh
+++ python/test_gui/fromwin.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cp  "/mnt/c/users/perni/OneDrive/Documents/PythonTest/test_PySimpleGui.py" . 
 cp  "/mnt/c/users/perni/OneDrive/Documents/PythonTest/test_gui.xlsx" . 
+cp  "/mnt/c/users/perni/OneDrive/Documents/PythonTest/test_tkinter.py" . 
